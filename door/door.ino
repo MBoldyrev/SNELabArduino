9b0beb010cc1cb5a18fb31c7bbdc72539997488b
mboldyrev <mikael-1994@ya.ru>
2017-02-18 22:55:53 +0300

    Sat Feb 18 22:55:53 MSK 2017

--- door/door.ino
+++ door/door.ino
@@ -1,14 +1,24 @@
 #include <SPI.h>
 #include "nRF24L01.h"
 #include "RF24.h"
+#include "stdio.h"
 
 int lock_pin = 2;
 
 RF24 radio(9,10);
 const uint64_t pipe = 0xE8E8F0F0E1LL;
+char cmd[8] = {0};
 
 void setup() {
   radio.begin();
+
+  radio.setPALevel(RF24_PA_LOW); // one of RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH and RF24_PA_MAX
+  radio.setDataRate(RF24_250KBPS); // RF24_250KBPS for 250kbs, RF24_1MBPS for 1Mbps, or RF24_2MBPS for 2Mbps
+  radio.setChannel(42); // Which RF channel to communicate on, 0-125
+  radio.setAutoAck(1);                     // Ensure autoACK is enabled
+  radio.setRetries(3, 30);                 // Optionally, increase the delay between retries & # of retries
+  radio.powerUp();
+  
   radio.openReadingPipe(1,pipe);
   radio.startListening();
   
@@ -18,15 +28,18 @@ void setup() {
 
 void loop() {
 // if there is data ready
-  uint8_t cmd;
   if ( radio.available() )
   {
     // Dump the payloads until we've gotten everything
     while (radio.available())
     {
       // Fetch the payload, and see if this was the last one.
-      radio.read( cmd, 1 );
-      if( cmd == 'o' ) {
+      radio.read( cmd, 8 );
+      Serial.write( "Got command...\r\n" );
+      Serial.println( cmd[0] );
+      delay(100);
+      Serial.flush();
+      if( cmd[0] == 'o' ) {
         Serial.write( "Opening...\r\n" );
         pinMode(lock_pin, OUTPUT);
         digitalWrite(lock_pin, LOW);
--- tx_door/door/door.ino
+++ tx_door/door/door.ino
@@ -3,13 +3,9 @@
 #include "nRF24L01.h"
 #include "RF24.h"
 
-RF24 radio(9,10);
+RF24 radio(9, 10);
 const uint64_t pipe1 = 0xE8E8F0F0E1LL;
-
-
-
-char command = 0;
-char *cmd = &command;
+char cmd[8] = {0xAA};
 
 void setup() {
   Serial.begin(9600);
@@ -17,28 +13,34 @@ void setup() {
   // Setup radio pipe
   //
   radio.begin();
-  
+
   radio.setPALevel(RF24_PA_LOW); // one of RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH and RF24_PA_MAX
   radio.setDataRate(RF24_250KBPS); // RF24_250KBPS for 250kbs, RF24_1MBPS for 1Mbps, or RF24_2MBPS for 2Mbps
   radio.setChannel(42); // Which RF channel to communicate on, 0-125
   radio.setAutoAck(1);                     // Ensure autoACK is enabled
-  radio.setRetries(3,30);                  // Optionally, increase the delay between retries & # of retries
+  radio.setRetries(3, 30);                 // Optionally, increase the delay between retries & # of retries
   radio.powerUp();
-  
+
   radio.openWritingPipe(pipe1);
+
+  radio.printDetails();
 }
 
 void loop() {
-    if ( Serial.available() > 0 ) {
-        *cmd = Serial.read();
-        if( *cmd == 'o' ) {
-            Serial.write( "Sent opening command...\r\n" );
-            while  ( !radio.write( cmd, sizeof(char) ) );
-
-    //   pinMode(lock_pin, OUTPUT);
-    //   digitalWrite(lock_pin, LOW);
-    //   delay(1000);
-    //   pinMode(lock_pin, INPUT);
-    }
+  
+  if ( Serial.available() > 0 ) {
+    cmd[0] = Serial.read();
+//    if ( cmd[0] == 'o' ) {
+      //while  ( !radio.write( &cmd, 8 ) );
+    radio.write( cmd, 8 );
+    delay(100);
+    Serial.write( "Sent opening command...\r\n" );
+    Serial.println( cmd[0] );
+    Serial.flush();
+      //   pinMode(lock_pin, OUTPUT);
+      //   digitalWrite(lock_pin, LOW);
+      //   delay(1000);
+      //   pinMode(lock_pin, INPUT);
+//    }
   }
 }
